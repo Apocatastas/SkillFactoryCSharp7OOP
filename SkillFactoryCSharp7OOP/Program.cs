eb19293a66caeedf78c28b81da9052b9efc6120b
ɐllɐ <molchanova.a.a@gmail.com>
2024-09-28 13:09:54 +0300

    task 7.2.12

--- SkillFactoryCSharp7OOP/Program.cs
+++ SkillFactoryCSharp7OOP/Program.cs
@@ -1,45 +1,28 @@
 ﻿using System;
+using System.Numerics;
 
 class Program
 {
-    public class A
+    class Obj
     {
-        public virtual void Display()
-        {
-            Console.WriteLine("A");
-        }
-
-    }
-    public class B : A 
-    {
-        public new void Display()
-        {
-            Console.WriteLine("B");
-        }
-    }
-    public class C : A
-    {
-        public override void Display()
-        {
-            Console.WriteLine("C");
-        }
-    }
-    public class D : B
-    {
-        public new void Display()
-        {
-            Console.WriteLine("D");
-        }
-    }
-    public class E : C
-    {
-        public new void Display()
-        {
-            Console.WriteLine("E");
-        }
+        public int Value;
     }
 
+    static Obj operator +(Obj a, Obj b)
+    {
+        return new Obj
+        {
+            Value = a.Value + b.Value
+        };
+    }
 
+    static Obj operator -(Obj a, Obj b)
+    {
+        return new Obj
+        {
+            Value = a.Value - b.Value
+        };
+    }
 
     public static void Main(string[] args)
     {
