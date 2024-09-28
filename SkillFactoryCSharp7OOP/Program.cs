6cbf56ea5060658bb79e9fc5a1b6ce22981f8289
ɐllɐ <molchanova.a.a@gmail.com>
2024-09-28 12:47:23 +0300

    task 7.27

--- SkillFactoryCSharp7OOP/Program.cs
+++ SkillFactoryCSharp7OOP/Program.cs
@@ -2,84 +2,48 @@
 
 class Program
 {
-    class BaseClass
+    public class A
     {
-        protected string Name;
-
-        public virtual int Counter
+        public virtual void Display()
         {
-            get;
-            set;
+            Console.WriteLine("A");
         }
 
-        public BaseClass()
+    }
+    public class B : A 
+    {
+        public new void Display()
         {
-           
+            Console.WriteLine("B");
         }
-
-        public BaseClass(string name)
+    }
+    public class C : A
+    {
+        public override void Display()
         {
-            Name = name;
+            Console.WriteLine("C");
         }
-
-        virtual public void Display()
+    }
+    public class D : B
+    {
+        public new void Display()
         {
-            Console.WriteLine("Метод класса BaseClass");
+            Console.WriteLine("D");
+        }
+    }
+    public class E : C
+    {
+        public new void Display()
+        {
+            Console.WriteLine("E");
         }
     }
 
-    class DerivedClass : BaseClass
-    {
-        public string Description;
 
-        public int counter;
-
-        public override int Counter
-        {
-            get
-            {
-                return counter;
-            }
-            
-            set
-            {
-                if (value < 0)
-                {
-                    Console.WriteLine("Счётчик не может быть отрицательным");
-                }
-                else
-                {
-                    counter = value;
-                }
-            }
-        }
-
-        override public void Display()
-        {
-            base.Display();
-            Console.WriteLine("Метод класса DerivedClass");
-        }
-
-        public DerivedClass()
-        { 
-           
-        }
-
-        public DerivedClass(string name, string description) : base(name)
-        {
-            Description = description;
-        }
-
-        public DerivedClass(string name, string description, int counter): this(name,description)
-        {
-            Counter = counter;
-        }
-    }
 
     public static void Main(string[] args)
     {
-        DerivedClass obj = new DerivedClass();
-        obj.Display();
+
         Console.ReadKey();
     }
 }
