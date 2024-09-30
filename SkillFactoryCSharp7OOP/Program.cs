b9948b94280192a6d7abd3b8eda9f6d4c876b7be
ɐllɐ <molchanova.a.a@gmail.com>
2024-09-30 21:30:04 +0300

    task 7.3.3

--- SkillFactoryCSharp7OOP/Program.cs
+++ SkillFactoryCSharp7OOP/Program.cs
@@ -3,41 +3,45 @@ using System.Numerics;
 
 class Program
 {
-    class IndexingClass
+    /*
+     Создайте классы для следующих объектов компьютера:
+    процессор (Processor),
+    материнская карта (MotherBoard),
+    видеокарта (GraphicCard). Унаследуйте их от класса ComputerPart.
+
+Добавьте в класс ComputerPart абстрактный метод Work без параметров и с типом void.
+     */
+
+    public class Processor : ComputerPart
     {
-        private int[] array;
-
-        public IndexingClass(int[] array)
+        public override void Work()
         {
-            this.array = array;
 
         }
+    }
 
-        // Индексатор по массиву
-        public int this[int index]
+    public class MotherBoard : ComputerPart
+    {
+        public override void Work()
         {
-            get
-            {
-                return array[index];
-            }
 
-            set
-            {
-                array[index] = value;
-            }
         }
+    }
 
+    public class GraphicCard : ComputerPart
+    {
+        public override void Work()
+        {
 
+        }
     }
 
 
-
-
-
-
-
-
-
+    public abstract class ComputerPart
+    {
+        public abstract void Work();
+    }
+   
     public static void Main(string[] args)
     {
 
