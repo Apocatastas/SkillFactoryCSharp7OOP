4d1a63d0cd5824e0f5e73498b622959cfe9be512
ɐllɐ <molchanova.a.a@gmail.com>
2024-09-30 21:59:54 +0300

    task 7.5.5

--- SkillFactoryCSharp7OOP/Program.cs
+++ SkillFactoryCSharp7OOP/Program.cs
@@ -3,31 +3,29 @@ using System.Numerics;
 
 class Program
 {
-    /*
-    Создайте класс Helper и определите в нем статический метод Swap типа void,
-    который принимает 2 переменные типа int и меняет их значения местами.
-     */
 
-    public class Helper
+    class Obj
     {
-        public static void Swap(ref int a, ref int b)
+        public string Name;
+        public string Description;
+
+        public static string Parent;
+        public static int DaysInWeek;
+        public static int MaxValue;
+
+        Obj()
         {
-            int buffer = a;
-            a = b;
-            b = buffer;
+        Parent = "System.Object";
+        DaysInWeek = 7;
+        MaxValue = 2000;
 
-        }
     }
+}
 
-   
+
     public static void Main(string[] args)
     {
-        int num1 = 3;
-        int num2 = 58;
 
-        Helper.Swap(ref num1, ref num2);
-        Console.WriteLine(num1); //58
-        Console.WriteLine(num2); //3
         Console.ReadKey();
     }
 }
