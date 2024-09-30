983226b94c277c64566e626cf8722b233f500165
ɐllɐ <molchanova.a.a@gmail.com>
2024-09-30 21:45:41 +0300

    task 7.5.2

--- SkillFactoryCSharp7OOP/Program.cs
+++ SkillFactoryCSharp7OOP/Program.cs
@@ -4,43 +4,18 @@ using System.Numerics;
 class Program
 {
     /*
-     Создайте классы для следующих объектов компьютера:
-    процессор (Processor),
-    материнская карта (MotherBoard),
-    видеокарта (GraphicCard). Унаследуйте их от класса ComputerPart.
-
-Добавьте в класс ComputerPart абстрактный метод Work без параметров и с типом void.
+    Создайте класс Obj, определите в нем поля
+    Name, Description (тип строки) и статическое поле MaxValue типа int, равное 2000.
      */
 
-    public class Processor : ComputerPart
+    public class Obj
     {
-        public override void Work()
-        {
+        public string Name;
+        public string Description;
+        public static int MaxValue = 2000;
 
-        }
     }
 
-    public class MotherBoard : ComputerPart
-    {
-        public override void Work()
-        {
-
-        }
-    }
-
-    public class GraphicCard : ComputerPart
-    {
-        public override void Work()
-        {
-
-        }
-    }
-
-
-    public abstract class ComputerPart
-    {
-        public abstract void Work();
-    }
    
     public static void Main(string[] args)
     {
