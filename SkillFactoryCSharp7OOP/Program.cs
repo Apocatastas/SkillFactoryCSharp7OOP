3ebd994704ec14738e31165feb9ff47358cca063
ɐllɐ <molchanova.a.a@gmail.com>
2024-10-02 21:31:32 +0300

    task 7.6.6

--- SkillFactoryCSharp7OOP/Program.cs
+++ SkillFactoryCSharp7OOP/Program.cs
@@ -3,25 +3,17 @@ using System.Numerics;
 
 
 /*
-Создайте класс-обобщение Car для автомобиля.
-Универсальным параметром будет тип двигателя в автомобиле (электрический и бензиновый).
-Для типов двигателей также создайте классы — ElectricEngine и GasEngine.
-
-В классе Car создайте поле Engine в качестве типа которому укажите универсальный параметр.
+Реализуйте класс-обобщение Record, у которого будут два универсальных параметра:
+один — для идентификатора записи (Id),
+другой — для значения записи (Value).
+Также в классе реализуйте поле Date типа DateTime.
 */
 
 
-public class Car <T>
+public class Record <T1, T2>
     {
-    public T Engine;
+    public T1 Id;
+    public T2 Value;
+    public DateTime Date;
     }
 
-public class ElectricEngine
-    {
-
-    }
-
-public class GasEngine
-{
-
-}
